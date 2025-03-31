8c153e9fe2e538125d028c29193cc5e86f25f436
bendico <73071322+bendico12@users.noreply.github.com>
2025-03-31 15:19:48 +0900

    Update script2.js

--- js/script2.js
+++ js/script2.js
@@ -83,7 +83,7 @@ window.addEventListener('scroll', function() {
   });
 
 $(function() {
-    $('.ft_title').hover(
+    $('.ft_title').click(
         function() {
             if ($(window).width() <= 425) {
                 $(this).siblings('.ft_sub').stop(true, true).slideDown(); // 슬라이드 다운
